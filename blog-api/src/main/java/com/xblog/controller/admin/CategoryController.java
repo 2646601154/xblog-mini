965565fd5ed5a736e6b5b2c1d2cfee26ac718ad1
Xiaruoxin <2646601154@qq.com>
2026-05-01 20:00:55 +0800

    feat(category): 完成分类管理 CRUD 接口

--- blog-api/src/main/java/com/xblog/controller/admin/CategoryController.java
+++ blog-api/src/main/java/com/xblog/controller/admin/CategoryController.java
@@ -1,6 +1,7 @@
 package com.xblog.controller.admin;
 
 import com.xblog.dto.CategoryCreateParam;
+import com.xblog.dto.CategoryUpdateParam;
 import com.xblog.entity.Result;
 import com.xblog.service.CategoryService;
 import com.xblog.vo.CategoryAdminVo;
@@ -26,15 +27,44 @@ public class CategoryController {
      * @return
      */
     @GetMapping
-    public Result<List<CategoryAdminVo>> getCategoryList(){
+    public Result<List<CategoryAdminVo>> getCategoryList() {
         log.info("获取管理分类列表");
         return Result.success(categoryService.getAdminCategoryList());
     }
 
-    //创建分类
+    /**
+     * 创建分类
+     *
+     * @param param
+     * @return
+     */
     @PostMapping
-    public Result<CategoryPublicVo> createCategory(@Valid @RequestBody CategoryCreateParam param){
+    public Result<CategoryPublicVo> createCategory(@Valid @RequestBody CategoryCreateParam param) {
         log.info("创建分类: {}", param.getName());
         return Result.success(categoryService.createCategory(param));
+    }
+
+    /**
+     * 更新分类
+     * @param id
+     * @param param
+     * @return
+     */
+    @PutMapping("/{id}")
+    public Result<CategoryPublicVo> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryUpdateParam param) {
+        log.info("更新分类: id={}, name={}", id, param.getName());
+        return Result.success(categoryService.updateCategory(id, param));
+    }
+
+    /**
+     * 删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public Result<Void> deleteCategory(@PathVariable Long id) {
+        log.info("删除分类: id={}", id);
+        categoryService.deleteCategory(id);
+        return Result.success();
     }
 }
--- blog-api/src/main/java/com/xblog/service/CategoryService.java
+++ blog-api/src/main/java/com/xblog/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.xblog.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.xblog.dto.CategoryCreateParam;
+import com.xblog.dto.CategoryUpdateParam;
 import com.xblog.entity.Category;
 import com.xblog.vo.CategoryAdminVo;
 import com.xblog.vo.CategoryPublicVo;
@@ -13,4 +14,8 @@ public interface CategoryService extends IService<Category> {
     List<CategoryAdminVo> getAdminCategoryList();
 
     CategoryPublicVo createCategory(CategoryCreateParam param);
+
+    CategoryPublicVo updateCategory(Long id, CategoryUpdateParam param);
+
+    void deleteCategory(Long id);
 }
